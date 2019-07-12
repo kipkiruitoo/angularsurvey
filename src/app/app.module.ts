b32122eb2085ce1cc09d93e7cb4c4104241f7fe9
alfi3 <you@example.com>
2019-07-12 16:39:26 +0300

    created a n ew branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,27 @@ import { SurveyCreatorComponent } from "./admin/create-survey/survey.creator.com
 // import { CreateSurveyComponent } from "./admin/create-survey/create-survey.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { HomeComponent } from './home/home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [AppComponent, SurveyComponent, SurveyCreatorComponent, HomeComponent],
-
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      SurveyComponent,
+      SurveyCreatorComponent,
+      HomeComponent,
+      LoginComponent,
+      SignupComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      HttpClientModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
