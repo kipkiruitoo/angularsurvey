db0c9107b4f4b30310590a00421a6c9441a7c15d
gkarumba <gachegua@gmail.com>
2019-07-23 11:41:51 +0300

    Add user resgister/login

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,22 +1,47 @@
 import { NgModule } from "@angular/core";
 import { AppComponent } from "../app.component";
-import { HomeComponent } from "../home/home/home.component";
-import { SurveyComponent } from "../repondent/answer-survey/survey.component";
-import { SurveyCreatorComponent } from "../admin/create-survey/survey.creator.component";
+import { HomeComponent } from "../components/home/home/home.component";
+import { SurveyComponent } from "../components/respondent/answer-survey/survey.component";
+import { SurveyCreatorComponent } from "../components/admin/create-survey/survey.creator.component";
+import { LoginComponent } from "../components/auth/login/login.component";
+import { RegisterComponent } from "../components/auth/register/register.component";
+import { ProfileComponent } from "../components/usersPages/profile/profile.component"
+import { AdminComponent } from "../components/admin/admin/admin.component";
+
 import { RouterModule, Routes } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {
     path: "survey",
-    component: SurveyComponent
+    component: SurveyComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "admin",
+    component: AdminComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "profile",
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
   },
   {
     path: "",
     component: HomeComponent
   },
   {
-    path: "admin",
+    path: "createSurvey",
     component: SurveyCreatorComponent
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
-import { SurveyComponent } from "./repondent/answer-survey/survey.component";
-import { SurveyCreatorComponent } from "./admin/create-survey/survey.creator.component";
+import { SurveyComponent } from "./components/respondent/answer-survey/survey.component";
+import { SurveyCreatorComponent } from "./components/admin/create-survey/survey.creator.component";
 // import { CreateSurveyComponent } from "./admin/create-survey/create-survey.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
-import { HomeComponent } from './home/home/home.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { VerifierComponent } from './components/usersPages/verifier/verifier.component';
+import { JudgeComponent } from './components/usersPages/judge/judge.component';
+import { ScripterComponent } from './components/usersPages/scripter/scripter.component';
+import { ProfileComponent } from './components/usersPages/profile/profile.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import {AuthService } from './services/auth.service';
+import { AuthGuard } from './guards/auth.guard';
+import { TokenInterceptorService } from './services/tokeninterceptor.service';
+import { AdminComponent } from './components/admin/admin/admin.component';
+import { AdminSidebarComponent } from './components/admin/admin-sidebar/admin-sidebar.component'
 
 @NgModule({
-  declarations: [AppComponent, SurveyComponent, SurveyCreatorComponent, HomeComponent],
+  declarations: [AppComponent, SurveyComponent, SurveyCreatorComponent, HomeComponent, LoginComponent, RegisterComponent, VerifierComponent, JudgeComponent, ScripterComponent, ProfileComponent, HeaderComponent, FooterComponent, AdminComponent, AdminSidebarComponent],
 
   imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
-  providers: [],
+  providers: [AuthService, AuthGuard,{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
