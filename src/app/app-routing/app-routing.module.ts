27352d51b0c7c3f3080fd5353c4ebba6024863e2
Davis Too <davis.too@strathmore.edu>
2019-08-09 10:01:17 +0300

    added # to solve the reload problem

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -73,7 +73,7 @@ const routes: Routes = [
 ];
 @NgModule({
   declarations: [],
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
